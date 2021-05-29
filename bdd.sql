d0de2055aae3b5189d3745263cb0f3e0afe77548
Ruben <74672236+rubenclerc@users.noreply.github.com>
2021-05-29 16:44:47 +0200

    Add SQL script
    This script creates the needed table.

---
+++ bdd.sql
@@ -0,0 +1,10 @@
+
+-- Table creation
+CREATE TABLE t_characters(
+c_name VARCHAR(75) PRIMARY KEY,
+c_vision VARCHAR(75),
+c_rarity INTEGER,
+c_weapon VARCHAR(75),
+c_nation VARCHAR(75),
+c_type CHAR(3)
+);
